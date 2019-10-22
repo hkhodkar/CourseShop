c7f0ce8230e74e04a6fb2cb9a00bdaa485617ffc
hatef <hatef@HATEF-PC>
2019-10-21 22:14:52 -0700

    add helper classes (password,fixedText,namegenerator)

---
+++ CourseShop/CourseShop.Core/DTO/AccountViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseShop.Core.DTO
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Username { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Email { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string Password { get; set; }
+
+        [Display(Name = " تکرار رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [Compare("Password",ErrorMessage ="رمز عبور با تکرار آن مغایرت دارد")]
+        public string Repassword { get; set; }
+
+    }
+}
---
+++ CourseShop/CourseShop.Core/Generators/NameGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseShop.Core.Generators
+{
+    public class NameGenerator
+    {
+        public static string GenerateUniqCode()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
