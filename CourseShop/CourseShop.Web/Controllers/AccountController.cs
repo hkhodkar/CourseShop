1d1cfb535e5751785fb035ff66b81c262f5b43ab
hatef <hatef@HATEF-PC>
2019-10-21 22:15:33 -0700

    add home and account controller + register and login

---
+++ CourseShop/CourseShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using CourseShop.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CourseShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Author : hatef khodkar
+        /// Created Date : 21/10/2019
+        /// Last Update : 21/10/2019
+        /// </summary>
+        /// <returns></returns>
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("Register")]
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return await Task.Run(() => View());
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return await Task.Run(() => View());
+
+            return await Task.Run(() => View());
+        }
+    }
+}
